a2ce5889bd0e7d2308b99608957d09e31038987c
Saurav Kumar Jha <122507083+jhajee22@users.noreply.github.com>
2023-09-29 01:15:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -34,6 +34,16 @@ const lookup = {
 function rot13(encodedStr) {
   let decodedArr = []; // Your Result goes here
   // Only change code below this line
+	 for (let i = 0; i < encodedStr.length; i++) {
+    const char = encodedStr[i];
+    if (lookup[char] !== undefined) {
+      decodedArr.push(lookup[char]);
+    } else {
+      decodedArr.push(char);
+    }
+  }
+  return decodedArr.join(""); // Join the characters to form the decoded string
+}
 
   return; //return decodedArr
 }
